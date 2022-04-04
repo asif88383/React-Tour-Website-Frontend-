b9703aa3df376eec7ef9f6d2cdcc0fa295eb6279
Muhammad Asif <asif88383@gmail.com>
2022-04-04 16:06:59 +0500

    first commit

--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
+
+  const [showInfo, setShowInfo] = useState(false);
   return (
     <article>
       <img src={image} alt={name} />
@@ -8,7 +10,11 @@ const Tour = ({id, image, info, price, name}) => {
         <div className='tour-info'>
           <h4>{name}</h4>
           <h4 className='tour-price'>{price}</h4>
-          <p>{info}</p>
+          <p>{showInfo ? info : `${info.substring(0,200)}...`}
+            <button onClick={() => setShowInfo(!showInfo)}>
+              {showInfo ? 'hide' : 'show'}
+            </button>
+          </p>
           <button className='delete-btn'>Not Interested</button>
         </div>
       </footer>
